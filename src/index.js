7d30bbb41de48e2b37cbc5c18946298b443691e2
hata-junnosuke <jh19911209@icloud.com>
2024-03-18 09:58:23 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,53 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
+
+// 複数のreducerをindexでまとめてallReducerとしてstoreを作成する.個別に追加することはできない。
+const store = createStore(allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// index.jsにまとめた記載。これを分割していく。
+// // actions -> increment, decrement
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+// // reducer -> アクションと前の状態を組み合わせて新しい状態を返す
+// const counterReducer = (state = 0, action) => {
+//   switch(action.type) {
+//     case 'INCREMENT':
+//       return state + 1
+//     case 'DECREMENT':
+//       return state - 1
+//   }
+// }
+
+// // store
+// let store = createStore(counterReducer);
+
+// // コンソールに新しい状態を出力
+// store.subscribe(() => console.log(store.getState()));
+
+// // dispatch
+// store.dispatch(increment());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>
 );
 
